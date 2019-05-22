a8182b8064387e2683f74f6c6e6d5f9e117ded4c
Leandro Venâncio <leandrohilariovenancio@gmail.com>
2019-05-22 01:17:08 -0300

    COMMIT-012 - Curso Laravel Alura Parte 2 - Trabalhando com autenticação, Relacionamento e Testes - Projeto Laravel - Controle Séries

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,8 +4,12 @@
 namespace App\Http\Controllers;
 
 
+use App\Episodio;
 use App\Http\Requests\SeriesFormRequest;
 use App\Serie;
+use App\Services\CriadorDeSerie;
+use App\Services\RemovedorDeSerie;
+use App\Temporada;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
@@ -24,19 +28,16 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(SeriesFormRequest $request)
-    {
-        $serie = Serie::create(['nome' => $request->nome]);
-        $qtdTemporadas = $request->qtd_temporadas;
-        for($i=1; $i <= $qtdTemporadas; $i++)
-        {
-            $temporada = $serie->temporadas()->create(['numero' => $i]);
-
-            for($j=1; $j <= $request->ep_por_temporada; $j++)
-            {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
-        }
+    public function store
+    (
+        SeriesFormRequest $request,
+        CriadorDeSerie $criadorDeSerie
+    ) {
+        $serie = $criadorDeSerie->criarSerie(
+            $request->nome,
+            $request->qtd_temporadas,
+            $request->ep_por_temporada
+        );
 
         $request->session()
             ->flash(
@@ -48,13 +49,13 @@ class SeriesController extends Controller
 
     }
 
-    public function destroy(Request $request)
+    public function destroy(Request $request, RemovedorDeSerie $removedorDeSerie)
     {
-        Serie::destroy($request->id);
+        $nomeSerie = $removedorDeSerie->removerSerie($request->id);
         $request->session()
             ->flash(
                 'mensagem',
-                "Série removida com sucesso!"
+                "Série $nomeSerie removida com sucesso!"
             );
 
         return redirect()->route('listar_series');
